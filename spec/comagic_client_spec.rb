58d6e578ab5798dd1151ca772e252556b433c7c7
Vovan <bluurn@gmail.com>
2015-02-17 15:21:23 +0300

    agency_account: spec for sites

--- spec/comagic_client_spec.rb
+++ spec/comagic_client_spec.rb
@@ -4,9 +4,7 @@ config = YAML::load_file(File.join(File.dirname(__FILE__ ), '..', 'secrets.yml')
 
 describe ComagicClient do
 
-
-  describe "login feature" do
-    describe "with valid credentials" do
+    describe "as an agency account" do
       let(:agency_connector) do
         login = config['accounts']['agency']['login']
         password = config['accounts']['agency']['password']
@@ -17,13 +15,22 @@ describe ComagicClient do
         expect(agency_connector.login).to be_an_instance_of(String)
       end
 
-      describe "logout feature" do
+      describe "when logged in" do
         before { agency_connector.login }
+
+        describe "site feature" do
+          it "should not get sites with no options specified" do
+            expect { agency_connector.site }.to raise_error(ComagicClient::ApiError, /Access denied/)
+          end
+          it "should get sites if customer_id specified" do
+            expect(agency_connector.site customer_id: config['accounts']['agency']['customer_id']).not_to be_empty
+          end
+        end
+
         it "should logout from service" do
           expect(agency_connector.logout).to be_empty
         end
       end
-
     end
 
     describe "with invalid credentials" do
@@ -35,5 +42,4 @@ describe ComagicClient do
         expect { invalid_connector.login }.to raise_error(ComagicClient::ApiError, /Wrong login or password/)
       end
     end
-  end
 end
