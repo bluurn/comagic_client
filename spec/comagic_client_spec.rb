e897ea93caed94a0ffdc20b47d40c044ec06710f
Vovan <bluurn@gmail.com>
2015-02-17 15:49:27 +0300

    agency_account: communications

--- spec/comagic_client_spec.rb
+++ spec/comagic_client_spec.rb
@@ -23,7 +23,7 @@ describe ComagicClient do
           expect { agency_connector.site }.to raise_error(ComagicClient::ApiError, /Access denied/)
         end
         it "should get sites if customer_id specified" do
-          expect(agency_connector.site customer_id: config['accounts']['agency']['customer_id']).not_to be_empty
+          expect(agency_connector.site customer_id: config['accounts']['agency']['customer_id']).to be_a(Array)
         end
       end
 
@@ -32,13 +32,13 @@ describe ComagicClient do
           expect { agency_connector.ac }.to raise_error(ComagicClient::ApiError, /Access denied/)
         end
         it 'should return sites if customer_id is specified' do
-          expect(agency_connector.ac customer_id: config['accounts']['agency']['customer_id']).not_to be_empty
+          expect(agency_connector.ac customer_id: config['accounts']['agency']['customer_id']).to be_a(Array)
         end
         it 'should return sites if only site_id is specified' do
           expect { agency_connector.ac site_id: config['accounts']['agency']['site_id'] }.to raise_error(ComagicClient::ApiError, /Access denied/)
         end
         it 'should return sites if customer_id and site_id are specified' do
-          expect(agency_connector.ac site_id: config['accounts']['agency']['site_id'], customer_id: config['accounts']['agency']['customer_id']).not_to be_empty
+          expect(agency_connector.ac site_id: config['accounts']['agency']['site_id'], customer_id: config['accounts']['agency']['customer_id']).to be_a(Array)
         end
       end
 
@@ -48,8 +48,19 @@ describe ComagicClient do
           expect{ agency_connector.tag }.to raise_error(ComagicClient::ApiError, /Access denied/)
         end
         it 'should get tags if customer_id is specified' do
-          expect(agency_connector.tag customer_id: config['accounts']['agency']['customer_id']).not_to be_empty
+          expect(agency_connector.tag customer_id: config['accounts']['agency']['customer_id']).to be_a(Array)
         end
+      end
+
+      describe "communications feature" do
+        it 'should throw error with no options specified' do
+          expect { agency_connector.communication }.to raise_error(ComagicClient::ApiError, /Missing required parameter/)
+        end
+
+        it 'get communications if all parameters given' do
+          expect(agency_connector.communication customer_id: config['accounts']['agency']['customer_id'], site_id: config['accounts']['agency']['site_id'], date_from: config['accounts']['agency']['date_from'], date_till: config['accounts']['agency']['date_from']).to be_a Array
+        end
+
       end
 
       it "should logout from service" do
